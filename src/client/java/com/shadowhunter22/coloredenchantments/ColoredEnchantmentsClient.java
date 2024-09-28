26533840a3a7c3e6fca278327656e38c8e7152c3
shadowhunt22 <155584978+shadowhunt22@users.noreply.github.com>
2024-09-27 21:12:33 -0600

    initial commit

---
+++ src/client/java/com/shadowhunter22/coloredenchantments/ColoredEnchantmentsClient.java
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2024 by ShadowHunter22. All rights reserved.
+// See LICENSE file in the project root for details.
+//
+
+package com.shadowhunter22.coloredenchantments;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColoredEnchantmentsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/com/shadowhunter22/coloredenchantments/ColoredEnchantments.java
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2024 by ShadowHunter22. All rights reserved.
+// See LICENSE file in the project root for details.
+//
+
+package com.shadowhunter22.coloredenchantments;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ColoredEnchantments implements ModInitializer {
+	public static final String MOD_ID = "colored-enchantments";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Successfully loaded {} mod!", MOD_ID);
+	}
+}
