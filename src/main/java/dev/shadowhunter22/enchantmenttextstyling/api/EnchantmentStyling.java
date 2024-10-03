14269c46796765f32b6c9953f3e047385f9cfc3b
shadowhunter22 <155584978+shadowhunt22@users.noreply.github.com>
2024-10-03 16:57:18 -0600

    - Bug fix (PR #6)
    - Bump version
    
    - Added a README
    
    - Updated CODEC formats to conform to what the Registry expects
    
    - Fixed a bug where the client never truly received stylings from the resource loader, prompting me to create a RegistryKey and make changes to the CODEC

--- src/main/java/dev/shadowhunter22/enchantmenttextstyling/api/EnchantmentStyling.java
+++ src/main/java/dev/shadowhunter22/enchantmenttextstyling/api/EnchantmentStyling.java
@@ -14,13 +14,14 @@ import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-public record EnchantmentStyling(Identifier id, EnchantmentTextStyles styles, EnchantmentStylingCondition condition) {
+public record EnchantmentStyling(Identifier id, List<EnchantmentTextStyles> styles) {
     public static final Codec<EnchantmentStyling> CODEC = RecordCodecBuilder.create(builder -> builder.group(
             Identifier.CODEC.fieldOf("enchantment").forGetter(EnchantmentStyling::id),
-            EnchantmentTextStyles.CODEC.fieldOf("styles").forGetter(EnchantmentStyling::styles),
-            EnchantmentStylingCondition.CODEC.optionalFieldOf("conditions", EnchantmentStylingCondition.EMPTY).forGetter(EnchantmentStyling::condition)
+            EnchantmentTextStyles.CODEC.listOf().fieldOf("styles").forGetter(EnchantmentStyling::styles)
     ).apply(builder, EnchantmentStyling::new));
 
     public static Builder builder(RegistryKey<Enchantment> entry) {
@@ -93,27 +94,33 @@ public record EnchantmentStyling(Identifier id, EnchantmentTextStyles styles, En
         }
 
         public EnchantmentStyling build() {
+            ArrayList<EnchantmentTextStyles> list = new ArrayList<>();
+            list.add(
+                    new EnchantmentTextStyles(
+                            new EnchantmentStylingCondition(
+                                    Optional.ofNullable(this.value),
+                                    Optional.ofNullable(this.min),
+                                    Optional.ofNullable(this.max)
+                            ),
+                            this.color,
+                            Optional.ofNullable(this.bold),
+                            Optional.ofNullable(this.italic),
+                            Optional.ofNullable(this.underlined),
+                            Optional.ofNullable(this.strikethrough),
+                            Optional.ofNullable(this.obfuscated)
+                    )
+            );
+
             return new EnchantmentStyling(
                     this.id,
-                    new EnchantmentTextStyles(
-                        this.color,
-                        Optional.ofNullable(this.bold),
-                        Optional.ofNullable(this.italic),
-                        Optional.ofNullable(this.underlined),
-                        Optional.ofNullable(this.strikethrough),
-                        Optional.ofNullable(this.obfuscated)
-                    ),
-                    new EnchantmentStylingCondition(
-                        Optional.ofNullable(this.value),
-                        Optional.ofNullable(this.min),
-                        Optional.ofNullable(this.max)
-                    )
+                    list
             );
         }
     }
 
-    public record EnchantmentTextStyles(int color, Optional<Boolean> bold, Optional<Boolean> italic, Optional<Boolean> underlined, Optional<Boolean> strikethrough, Optional<Boolean> obfuscated) {
+    public record EnchantmentTextStyles(EnchantmentStylingCondition condition, int color, Optional<Boolean> bold, Optional<Boolean> italic, Optional<Boolean> underlined, Optional<Boolean> strikethrough, Optional<Boolean> obfuscated) {
         public static final Codec<EnchantmentTextStyles> CODEC = RecordCodecBuilder.create(builder -> builder.group(
+                EnchantmentStylingCondition.CODEC.optionalFieldOf("condition", EnchantmentStylingCondition.EMPTY).forGetter(EnchantmentTextStyles::condition),
                 Codec.INT.fieldOf("color").forGetter(EnchantmentTextStyles::color),
                 Codec.BOOL.optionalFieldOf("bold").forGetter(EnchantmentTextStyles::bold),
                 Codec.BOOL.optionalFieldOf("italic").forGetter(EnchantmentTextStyles::italic),
